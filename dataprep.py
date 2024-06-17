5ffcee41d848222dbbe9f95559ded765d083ef36
Olivia Cava <oliviacava@Olivias-MBP-6.hsd1.nh.comcast.net>
2024-06-17 17:38:03 -0400

    added 4 stimuli load and preprocess

--- dataprep.py
+++ dataprep.py
@@ -2,12 +2,48 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def mlp_load_and_preprocess_data(file_path, test_size=0.2, safe_data_fraction=0.1):
+def mlp4_load_and_preprocess_data(file_path, test_size=0.2, safe_data_fraction=0.1, undersample_glycerol = False):
+    data = pd.read_csv(file_path)
+    data.fillna(0, inplace=True)
+    data = data.iloc[:, :-13]  # drops the last 13 columns
+    mask_all_zeros = (data.iloc[:, 300:304].sum(axis=1) == 0)  # mask rows with all zeros in columns 300 to 304
+    data = data[~mask_all_zeros]
+
+    # Undersampling glycerol rows (only if called to do so)
+    if undersample_glycerol:
+        glycerol_rows = data[data['1Mglycerol'] == 1]
+        other_rows = data[data['1Mglycerol'] == 0]
+        sampled_glycerol_rows = glycerol_rows.sample(frac=0.5, random_state=42)
+        data = pd.concat([sampled_glycerol_rows, other_rows])
+        data = data.sample(frac=1, random_state=42).reset_index(drop=True)
+
+    X = data.iloc[:, :300]  # features
+    y = data.iloc[:, 300:304]  # predictors
+
+    # Training and safe data split
+    X_train, X_safe, y_train, y_safe = train_test_split(X, y, test_size=safe_data_fraction, random_state=42)
+
+    # Train test split on previous training
+    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=test_size, random_state=42)
+
+    return X_train, y_train, X_test, y_test, X_safe, y_safe
+
+
+def mlp_load_and_preprocess_data(file_path, test_size=0.2, safe_data_fraction=0.1, undersample_glycerol = False):
     data = pd.read_csv(file_path)
     data.fillna(0, inplace=True)
     data = data.iloc[:, :-13]  # drops the last 13 columns
     mask_all_zeros = (data.iloc[:, 300:310].sum(axis=1) == 0)  # mask rows with all zeros in columns 300 to 310
     data = data[~mask_all_zeros]
+
+    # Undersampling glycerol rows (only if called to do so)
+    if undersample_glycerol:
+        glycerol_rows = data[data['1Mglycerol'] == 1]
+        other_rows = data[data['1Mglycerol'] == 0]
+        sampled_glycerol_rows = glycerol_rows.sample(frac=0.5, random_state=42)
+        data = pd.concat([sampled_glycerol_rows, other_rows])
+        data = data.sample(frac=1, random_state=42).reset_index(drop=True)
+
     X = data.iloc[:, :300]  # features
     y = data.iloc[:, 300:310] # predictors
 
